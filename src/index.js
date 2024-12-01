c860c57d91df42fe95ce1561317beedd6309e90c
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-12-01 17:05:03 +0100

    Add ejs files
    Ejs files were created to separate pages for
    homepage, products and cart.

--- src/index.js
+++ src/index.js
@@ -9,3 +9,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
   createProductList.displayData("https://fakestoreapi.com/products?limit=20");
 });
+
+import homePage from "./pages/home.ejs";
+import detailsPage from "./pages/productDetails.ejs"
+import cartPage from "./pages/cart.ejs";
+
+
+window.addEventListener("popstate", () => {
+    renderPage(window.location.hash);
+})
+
+
+function renderPage(urlPath) {
+    if (urlPath == "#/cart-page") {
+        document.querySelector("body").innerHTML = cartPage();
+
+    } else if (urlPath.startsWith("#/products/")) {
+        let productId = urlPath.substring("#/products/".length)
+
+        fetch('https://fakestoreapi.com/products/' + productId)
+            .then(res=>res.json())
+            .then(json=> {
+                console.log(json)
+                document.querySelector("body").innerHTML = detailsPage({product: json});
+            })
+    } else {
+
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=> {
+                document.querySelector("body").innerHTML = homePage({products: json});
+            })
+    }
+}
+renderPage(window.location.hash);
